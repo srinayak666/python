fa4ebd6f8d95b0191e4acb995cd4cd95dcd8180d
sri <srinayak666@gmail.com>
2020-04-26 09:47:57 +0530

    READ ME

--- ComplexNumbers.py
+++
@@ -1,7 +0,0 @@
-
-#Python can also handle complex numbers and its associated functions using the file “cmath”.
-# Complex numbers have their uses in many applications related to mathematics and python provides useful tools to handle and manipulate them.
-
-#An complex number is represented by “ x + yi “.
-# Python converts the real numbers x and y into complex using the function complex(x,y).
-# The real part can be accessed using the function real() and imaginary part can be represented by imag().
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # python
-Experiments With Python
-#1 HelloWorld.py \n
-#2 Variables.py
-#3 StringOperations.py
-#4 List.py
+Experiments With Python <br/>
+1 HelloWorld.py <br/>
+2 Variables.py<br/>
+3 StringOperations.py<br/>
+4 List.py<br/>
+refer notes directory for Notes associated with above topics <br/>
+
+
