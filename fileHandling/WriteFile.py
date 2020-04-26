a900a8bb45d6975aa30e52ddad77c1f6891fc961
sri <srinayak666@gmail.com>
2020-04-26 10:29:07 +0530

    WriteFile in python

---
+++ fileHandling/WriteFile.py
@@ -0,0 +1,14 @@
+#Write to file
+fobj_in = open("fileHandling_Notes/WriteFile.txt")
+fobj_out = open("SampleTemp.txt","w")
+i = 1
+for line in fobj_in:
+    print(line.rstrip())
+    fobj_out.write(str(i) + ": " + line)
+    i = i + 1
+fobj_in.close()
+fobj_out.close()
+
+#writing with with statement
+with open("example.txt", "w") as fh:
+    fh.write("To write or not to write\nthat is the question!\n")
