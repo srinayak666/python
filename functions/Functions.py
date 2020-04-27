d181d393c70f7a99dd682a76e28b957b6674b034
sri <srinayak666@gmail.com>
2020-04-27 17:23:30 +0530

    functions in python

--- functions/Functions.py
+++ functions/Functions.py
@@ -8,8 +8,18 @@ print("Outside Function1") # this will executed first
 def function2(x):
     return 2*x
 
+def function3(x,y):
+    return x+y
+
+
+
 
 #calling function
 function1()
-a=function2(4)
-print(a)
+a=function2("4")
+x=function2(4)
+print(a,x)
+sum=function3(5,5)
+print(sum)
+# calling function2() without argumnet will cause below error:
+'''TypeError: function2() missing 1 required positional argument: 'x' '''
