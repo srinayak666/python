b635585e951d7e990969190ea3003ee67377d878
sri <srinayak666@gmail.com>
2020-04-27 21:30:24 +0530

    OOPs concepts

--- oop/classExample.py
+++ oop/classExample.py
@@ -28,3 +28,19 @@ txt3 = "My name is {}, I'am {}".format("John",36)
 # access the instance attributes
 print("{} is {} years old".format( blu.name, blu.age))
 print("{} is {} years old".format( woo.name, woo.age))
+
+'''
+In the above program, we create a class with name Parrot. 
+Then, we define attributes. 
+The attributes are a characteristic of an object.
+
+Then, we create instances of the Parrot class. 
+Here, blu and woo are references (value) to our new objects.
+
+Then, we access the class attribute using __class __.species.
+ Class attributes are same for all instances of a class. 
+ Similarly, we access the instance attributes using blu.name and blu.age. 
+ However, instance attributes are different for every instance of a class.
+
+
+'''
