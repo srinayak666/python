1720cddb47a911047849ee12e6b8ed0fcd9a6c87
sri <srinayak666@gmail.com>
2020-04-29 16:13:13 +0530

    Exception Handling

--- exceptionHandling/ExceptionHandling.py
+++ exceptionHandling/ExceptionHandling.py
@@ -10,7 +10,8 @@ except:
 #This means that the program will not terminate but instead will continue on to the next statements.
 
     if a_number < 0:
-      raise RuntimeError("You can't use a negative number")
+        print(a_number)
+     # raise RuntimeError("You can't use a negative number")
     else:
         print(math.sqrt(a_number))
         '''
@@ -18,3 +19,20 @@ except:
     raise RuntimeError("You can't use a negative number")
 RuntimeError: You can't use a negative number
         '''
+a_number=-99
+if a_number < 0:
+    print(a_number)
+    try:
+        a_number/0
+    except(RuntimeError,ValueError,ZeroDivisionError):
+        print("Error Occured")
+    else:
+        print(math.sqrt(a_number))
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
