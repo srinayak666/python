a6215752578eca6886a8f5abede8593b8d96247c
sri <srinayak666@gmail.com>
2020-05-04 16:37:56 +0530

    warm up examples

---
+++ examples/warmup/WarmpUp_front_times.py
@@ -0,0 +1,14 @@
+'''
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+
+'''
+def front_times(str, n):
+  if len(str) >= 3:
+    return str[0:3]*n
+  else:
+    return str*n
