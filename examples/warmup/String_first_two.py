e33d70f5c16c7283130f684541011d41ccf564bf
srinayak666 <srinayak666@gmail.com>
2020-05-06 15:58:30 +0530

    first two string example

---
+++ examples/warmup/String_first_two.py
@@ -0,0 +1,25 @@
+'''
+
+
+Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+
+
+first_two('Hello') → 'He'
+first_two('abcdefg') → 'ab'
+first_two('ab') → 'ab'
+'''
+
+def first_two(str):
+    if len(str) <2:
+        return str
+    else:
+        return str[0:2]
+
+
+print(first_two('Hello'))
+print(first_two('abcdefg'))
+print(first_two('H'))
+print(first_two('He'))
+print(first_two(''))
+    
